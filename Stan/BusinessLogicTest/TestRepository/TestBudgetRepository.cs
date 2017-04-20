7625fe7ecd45149ef59d29194b02e50d122fcca2
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2017-04-20 22:38:56 +0300

    Added Unit Tests to Stan folder for Code Review

---
+++ Stan/BusinessLogicTest/TestRepository/TestBudgetRepository.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic.Entities;
+using BusinessLogic.Repository;
+using BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicTest.TestRepository
+{
+    [TestClass]
+    public class TestBudgetRepository
+    {
+        Budget item = new Budget();
+        IBudgetRepository budgetRepo = new BudgetRepository();
+
+        [TestMethod]
+        public void Add_Test()
+        {
+            item.InitialBalance = 100;
+            item.StartDate = DateTime.Now;
+            item.EndDate = DateTime.Now;
+            item.Balance = item.InitialBalance;
+
+            budgetRepo.Add(item);
+        }
+
+        [TestMethod]
+        public void GetById_Test()
+        {
+            item.Id = 6;
+            item.InitialBalance = 100;
+            item.StartDate = DateTime.Now;
+            item.EndDate = DateTime.Now;
+            item.Balance = item.InitialBalance;
+
+            budgetRepo.GetById(item.Id);
+            Assert.IsNotNull(item);
+        }
+
+        [TestMethod]
+        public void GetAll_Test()
+        {
+            ICollection<Budget> budgets = new HashSet<Budget>();
+            budgets = budgetRepo.GetAll();
+
+            Assert.IsNotNull(budgets);
+            Assert.AreEqual(budgets.Count, budgetRepo.GetAll().Count);
+        }
+
+        [TestMethod]
+        public void Update_Test()
+        {
+            item.Id = 5;
+            item.InitialBalance = 100;
+            item.StartDate = DateTime.Now;
+            item.EndDate = DateTime.Now;
+            item.Balance = item.InitialBalance;
+
+            item.Balance = item.InitialBalance - 20;
+
+            budgetRepo.Update(item);
+        }
+
+        [TestMethod]
+        public void ChangeBalance_Test()
+        {
+            item.Id = 5;
+            item.InitialBalance = 100;
+            item.StartDate = DateTime.Now;
+            item.EndDate = DateTime.Now;
+            item.Balance = item.InitialBalance;
+
+            item.Balance = item.InitialBalance - 30;
+
+            budgetRepo.ChangeBalance(item);
+            Assert.IsNotNull(item);
+        }
+
+        [TestMethod]
+        public void GetBalance_Test()
+        {
+            item.Id = 5;
+            item.InitialBalance = 100;
+            item.StartDate = DateTime.Now;
+            item.EndDate = DateTime.Now;
+            item.Balance = item.InitialBalance;
+
+            budgetRepo.GetBalance(item);
+            Assert.IsNotNull(item);
+        }
+
+        [TestMethod]
+        public void Delete_Test()
+        {
+            item.Id = 5;
+            item.InitialBalance = 100;
+            item.StartDate = DateTime.Now;
+            item.EndDate = DateTime.Now;
+            item.Balance = item.InitialBalance;
+
+            budgetRepo.Delete(item);
+        }
+    }
+}
---
+++ Stan/BusinessLogicTest/TestRepository/TestDefaultCategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic.Entities;
+using BusinessLogic.Repository;
+using BusinessLogic.Interfaces;
+using System.Collections.Generic;
+
+namespace BusinessLogicTest.TestRepository
+{
+    [TestClass]
+    public class TestDefaultCategoryRepository
+    {
+        DefaultCategory item = new DefaultCategory();
+        Category categoryItem = new Category();
+        IDefaultCategoryRepository defaultCategoryRepo = new DefaultCategoryRepository();
+
+        [TestMethod]
+        public void GetAll_Test()
+        {
+            ICollection<DefaultCategory> defaultCategories = new HashSet<DefaultCategory>();
+
+            defaultCategories = defaultCategoryRepo.GetAll();
+
+            Assert.IsNotNull(defaultCategories);
+            Assert.AreEqual(defaultCategories.Count, defaultCategoryRepo.GetAll().Count);
+        }
+
+        [TestMethod]
+        public void GetById_Test()
+        {
+            item.Id = 1;
+            item.Name = "Car";
+
+            item = defaultCategoryRepo.GetById(item.Id);
+            Assert.IsNotNull(item);
+        }
+
+        [TestMethod]
+        public void GetByName_Test()
+        {
+            item.Id = 1;
+            item.Name = "Car";
+
+            categoryItem.Id = 1;
+            categoryItem.Name = "Car";
+            categoryItem.IsActive = true;
+            categoryItem.IsChosen = true;
+
+            defaultCategoryRepo.GetByName(categoryItem);
+            Assert.IsNotNull(categoryItem);
+        }
+    }
+}
