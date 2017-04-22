f3f38900c3026cc0bf065a9c4f1da4235c80152a
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2017-04-23 00:00:54 +0300

    Added new solution with new MVC project

---
+++ BudgetWatcher 23.04.2017/BusinessLogic/Interfaces/IBudgetRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic.Entities;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface IBudgetRepository : IGenericRepository<Budget>
+    {
+        decimal GetBalance(Budget item);
+        void ChangeBalance(Budget item);
+    }
+}
